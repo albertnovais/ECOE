a6b01ea8d958030a276f9fab6c8616fe2fcb80c9
albertnovais <albertnovais@gmail.com>
2019-08-18 22:40:26 -0300

    18/08

--- ECOE/App_Start/BundleConfig.cs
+++ ECOE/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace ECOE
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/DataTables/datatables.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/DataTables/datatables.min.css"));
         }
     }
 }
